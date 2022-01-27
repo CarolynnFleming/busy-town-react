3324b0b52a66fe6f52902c2be143fcd9fd7be68d
CarolynnFleming <cfcatpink@gmail.com>
2022-01-27 18:46:06 -0500

    stoplight

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -2,21 +2,17 @@ import React from 'react';
 
 export default function TrafficLight(props) {
   return (
-    // this component takes in a prop called color. It is either 'red', 'yellow' or 'green'.
+    // this component takes in a prop called color. It is either 'red', 'yellow' or 'green'.git 
     <div className='traffic-light'>
       {/* in each of these divs, set it up so that if the color matches, add the css class 'on' to the classNames */}
-      {props.color ? 'on' : 'off'}
       {/* for example, the css classes for the div below should be `red circle on`, if props.color is 'red' */}
-      <div className={`red circle on`}>
-        {props.on && 'if props.color is red'}
+      <div className={`red circle ${props.color === 'red' ? 'on' : ''}`}>
       </div>
       {/* the css classes for the div below should be `yellow circle on`, if props.color is 'yellow' */}
-      <div className={`yellow circle on`}>
-        {props.on && 'if props.color is yellow'}
+      <div className={`yellow circle ${props.color === 'yellow' ? 'on' : ''}`}>
       </div>
       {/*  the css classes for the div below should be `green circle on`, if props.color is 'green' */}
-      <div className={`green circle on`}>
-        {props.on && 'if props.color is green'}
+      <div className={`green circle ${props.color === 'green' ? 'on' : ''}`}>
       </div>
     </div>
   );
