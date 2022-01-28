97cf265257beb07dac6173c0e88365793327e5f0
CarolynnFleming <cfcatpink@gmail.com>
2022-01-27 19:09:56 -0500

    done

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,4 +1,4 @@
-export default function Vehicle({vehicle}) {
+export default function Vehicle({ vehicle }) {
   return <div className="vehicle">
     {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
     {vehicle === 'car' && '🚗'}
